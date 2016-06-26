2ae9402f1fa4b97627dead9edf9960e6c91246e7
Candelario Daniel Eguia <candelario.eguia@gmail.com>
2016-06-26 10:44:41 -0700

    added the Bitwise basics

--- Assembling/Assembling/Bitwise.h
+++ Assembling/Assembling/Bitwise.h
@@ -14,7 +14,28 @@ using namespace std;
 void bitwise() {
 	system("cls");
 	cout << "---------- Bitwise ---------\n\n";
-	int h, b, area, perim;
+	cout << "\tA quick demo of bitwise opperators in c++ :\n\n";
+	unsigned int a= 35, b = 17, c;
+	cout << "\tThe & (\"AND\") opperator\n";
+	c = a & b;
+	cout << "a = 35 = | 0 |\t| 0 |\t| 1 |\t| 0 |\t| 0 |\t| 0 |\t| 1 |\t| 1 |\n";
+	cout << "b = 17 = | 0 |\t| 0 |\t| 0 |\t| 1 |\t| 0 |\t| 0 |\t| 0 |\t| 1 |\n";
+	cout << "\t&_____________________________________________________________\n";
+	cout << "c = " << c << " = | 0 |\t| 0 |\t| 0 |\t| 0 |\t| 0 |\t| 0 |\t| 0 |\t| 1 |\n\n";
 
+	cout << "\tThe | (\"OR\") opperator\n";
+	c = a | b;
+	cout << "a = 35 = | 0 |\t| 0 |\t| 1 |\t| 0 |\t| 0 |\t| 0 |\t| 1 |\t| 1 |\n";
+	cout << "b = 17 = | 0 |\t| 0 |\t| 0 |\t| 1 |\t| 0 |\t| 0 |\t| 0 |\t| 1 |\n";
+	cout << "\t|_____________________________________________________________\n";
+	cout << "c = " << c << " = | 0 |\t| 0 |\t| 1 |\t| 1 |\t| 0 |\t| 0 |\t| 1 |\t| 1 |\n\n";
+
+	cout << "\tThe ~ (\"Compliment\") opperator\n";
+	c = ~a;
+	cout << "a = 35 = | 0 |\t| 0 |\t| 1 |\t| 0 |\t| 0 |\t| 0 |\t| 1 |\t| 1 |\n";
+	cout << "\t~_____________________________________________________________\n";
+	cout << "c = " << c << " = | 1 |\t| 1 |\t| 0 |\t| 1 |\t| 1 |\t| 1 |\t| 0 |\t| 0 |\n\n";
+	cout << "a in hex:\t\t" << hex << a << endl;
+	cout << "~a in hex:\t" << c << endl;
 	system("pause");
 }
