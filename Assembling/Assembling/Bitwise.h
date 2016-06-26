0da7ae1f008062535b679b9f3c477794bae2b419
Candelario Daniel Eguia <candelario.eguia@gmail.com>
2016-06-26 11:23:26 -0700

    Bitwise Explained

--- Assembling/Assembling/Bitwise.h
+++ Assembling/Assembling/Bitwise.h
@@ -38,4 +38,46 @@ void bitwise() {
 	cout << "a in hex:\t\t" << hex << a << endl;
 	cout << "~a in hex:\t" << c << endl;
 	system("pause");
+	cout << endl;
+	cout << "\tThe << (\"left shift\") opperator\n";
+	cout << "Using << 1 is the quickest way to multiply by 2.\n\n";
+	a = 4;
+	c = a << 1;
+	cout << dec;
+	cout << "a = 4 = | 0 |\t| 0 |\t| 0 |\t| 0 |\t| 0 |\t| 1 |\t| 0 |\t| 0 |\n";
+	cout << "   << 1 _____________________________________________________________\n";
+	cout << "c = " << c << " = | 0 |\t| 0 |\t| 0 |\t| 0 |\t| 1 |\t| 0 |\t| 0 |\t| 0 |\n\n";
+	
+	cout << "\nUsing << 2 is the quickest way to multiply by 4.\n\n";
+	c = a << 2;
+	cout << "a = 4 = | 0 |\t| 0 |\t| 0 |\t| 0 |\t| 0 |\t| 1 |\t| 0 |\t| 0 |\n";
+	cout << "   << 2 _____________________________________________________________\n";
+	cout << "c = " << c << " = | 0 |\t| 0 |\t| 0 |\t| 1 |\t| 0 |\t| 0 |\t| 0 |\t| 0 |\n\n";
+
+	cout << "\nSee it working with 5 = 0 1 0 1.\n\n";
+	a = 5;
+	c = a << 1;
+	cout << "a = 5 = | 0 |\t| 0 |\t| 0 |\t| 0 |\t| 0 |\t| 1 |\t| 0 |\t| 1 |\n";
+	cout << "   << 1 _____________________________________________________________\n";
+	cout << "c = " << c << " = | 0 |\t| 0 |\t| 0 |\t| 0 |\t| 1 |\t| 0 |\t| 1 |\t| 0 |\n\n";
+	system("pause");
+	
+	cout << endl;
+	cout << "\tThe >> (\"right shift\") opperator\n\n";
+	cout << "Using >> is the quickest way to devide with the result rounded down.\n\n";
+	a = 4;
+	c = a >> 1;
+	cout << dec;
+	cout << "a = 4 = | 0 |\t| 0 |\t| 0 |\t| 0 |\t| 0 |\t| 1 |\t| 0 |\t| 0 |\n";
+	cout << "   >> 1 _____________________________________________________________\n";
+	cout << "c = " << c << " = | 0 |\t| 0 |\t| 0 |\t| 0 |\t| 0 |\t| 0 |\t| 1 |\t| 0 |\n\n";
+	
+	cout << "See it working with 5 = 0 1 0 1.\n\n";
+	a = 5;
+	c = a >> 1;
+	cout << "a = 5 = | 0 |\t| 0 |\t| 0 |\t| 0 |\t| 0 |\t| 1 |\t| 0 |\t| 1 |\n";
+	cout << "   >> 1 _____________________________________________________________\n";
+	cout << "c = " << c << " = | 0 |\t| 0 |\t| 0 |\t| 0 |\t| 0 |\t| 0 |\t| 1 |\t| 0 |\n\n";
+
+	system("pause");
 }
