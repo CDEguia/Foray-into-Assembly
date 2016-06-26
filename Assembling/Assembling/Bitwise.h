e98ead253cb0e5a9fe702ad164ec1a4ebf063839
Candelario Daniel Eguia <candelario.eguia@gmail.com>
2016-06-26 10:25:04 -0700

    Added Bitwise.h template

--- Assembling/Assembling/Bitwise.h
+++ Assembling/Assembling/Bitwise.h
@@ -1,1 +1,20 @@
 #pragma once
+//----------------------------------------------------------
+//		Name		Candelario Eguia
+//		Course		CMPS-371
+//		Project		No.3 part I
+//		Due date	June 26, 2016
+//		Professor	Ray Ahmadnia
+//
+//   This program displays:
+//
+//----------------------------------------------------------
+using namespace std;
+
+void bitwise() {
+	system("cls");
+	cout << "---------- Bitwise ---------\n\n";
+	int h, b, area, perim;
+
+	system("pause");
+}
